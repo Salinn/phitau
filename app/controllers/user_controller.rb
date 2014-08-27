4e123b36ac3f475869d2b25b5104e92941e58169
Paul Darragh <pmd6624@rit.edu>
2014-08-27 16:46:04 -0400

    added role to users

---
+++ app/controllers/user_controller.rb
@@ -0,0 +1,43 @@
+class UserController < ApplicationController
+  load_and_authorize_resource
+
+  def index
+  end
+
+  def new
+  end
+
+  def show
+  end
+
+  def create
+    if @user.save
+      flash[:notice] = "Successfully created User." 
+      redirect_to root_path
+    else
+      render :action => 'new'
+    end
+  end
+
+  def edit
+  end
+
+  def update
+    params[:user].delete(:password) if params[:user][:password].blank?
+    params[:user].delete(:password_confirmation) if params[:user][:password].blank? and params[:user][:password_confirmation].blank?
+    if @user.update_attributes(params[:user])
+      flash[:notice] = "Successfully updated User."
+      redirect_to user_index_path
+    else
+      render :action => 'edit'
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      flash[:notice] = "Successfully deleted User."
+      redirect_to user_index_path
+    end
+  end
+
+end
