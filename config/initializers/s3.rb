0b337378c547662f54d984044a5dc13e642f2574
Paul Darragh <pmd6624@rit.edu>
2014-09-10 14:49:32 -0400

    should fix heroku carrierwave issue

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,5 @@
 CarrierWave.configure do |config|
+  config.storage = :fog
   config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
@@ -6,4 +7,6 @@ CarrierWave.configure do |config|
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
   config.fog_directory  = ENV['S3_BUCKET']
+  config.fog_public     = true
+  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
 end
