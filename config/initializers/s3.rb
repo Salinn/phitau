508edbf257cf4cf2181c2ba381faf1d4898875b2
Paul Darragh <pmd6624@rit.edu>
2014-09-10 15:03:12 -0400

    another attempt at heroku accepting fog

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,12 +1,20 @@
 CarrierWave.configure do |config|
-  config.storage = :fog
   config.fog_credentials = {
       :provider               => 'AWS',
       :aws_access_key_id      => ENV['S3_KEY'],
       :aws_secret_access_key  => ENV['S3_SECRET']
       # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
   }
+  if Rails.env.test? || Rails.env.development?
+    config.storage = :file
+    config.enable_processing = false
+    config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
+  else
+    config.storage = :fog
+  end
+
+  config.cache_dir = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
   config.fog_directory  = ENV['S3_BUCKET']
-  config.fog_public     = true
-  config.fog_attributes = {'Cache-Control' => 'max-age=315576000'}
+  config.fog_public     = false
+  config.fog_attributes = {}
 end
