e452535ae86d789e5f00092ec457b4fffbfbf249
Paul Darragh <pmd6624@rit.edu>
2014-09-14 14:43:58 -0400

    fixed fog setup

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,20 +1,19 @@
 CarrierWave.configure do |config|
-  config.fog_credentials = {
-      :provider               => 'AWS',
-      :aws_access_key_id      => ENV['S3_KEY'],
-      :aws_secret_access_key  => ENV['S3_SECRET']
-      # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
-  }
   if Rails.env.test? || Rails.env.development?
     config.storage = :file
     config.enable_processing = false
     config.root = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
   else
+    config.fog_credentials = {
+        :provider               => 'AWS',
+        :aws_access_key_id      => ENV['S3_KEY'],
+        :aws_secret_access_key  => ENV['S3_SECRET']
+        # :region                 => ENV['S3_REGION'] # Change this for different AWS region. Default is 'us-east-1'
+    }
     config.storage = :fog
+    config.fog_directory  = ENV['S3_BUCKET']
+    config.fog_public     = false
+    config.fog_attributes = {}
   end
-
   config.cache_dir = "#{Rails.root}/tmp/uploads/#{DateTime.now.to_f}.#{rand(999)}.#{rand(999)}"
-  config.fog_directory  = ENV['S3_BUCKET']
-  config.fog_public     = false
-  config.fog_attributes = {}
 end
