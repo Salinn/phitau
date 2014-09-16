2327b039dfd479a4de943bc8af2dc61707590327
Paul Darragh <pmd6624@rit.edu>
2014-09-16 14:31:25 -0400

    users can now request to become members

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -4,19 +4,23 @@ class UserController < ApplicationController
   end
 
   def new
-    @user.role = "user"
+    @user = User.new
   end
 
   def show
   end
 
   def create
-    @user.role = "Nothing"
-    if @user.save
-      flash[:notice] = "Successfully created User." 
-      redirect_to root_path
-    else
-      render :action => 'new'
+    @user = User.new(post_params)
+    @user.role = "new_user"
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render action: 'show', status: :created, location: @user }
+      else
+        format.html { render action: 'new' }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
     end
   end
 
