a5e5a0cc1fc0e6494632a94dcf1caf52e48b2ad2
Paul Darragh <pmd6624@rit.edu>
2014-11-05 15:27:19 -0500

    fixed a small production issue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,4 @@ Phitau::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.middleware.use Rack::TwilioWebhookAuthentication, Rails.application.secrets.twilio_auth_token, '/voice'
 end
