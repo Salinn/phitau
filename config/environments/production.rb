e9b138050603e802be238b8038ab2348d36ed273
Paul Darragh <pmd6624@rit.edu>
2014-12-12 21:59:32 -0500

    updated mailer to be configurable for preprod and prod

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,7 @@ Phitau::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  config.action_mailer.default_url_options = { :host => 'gammanu.org' }
+  config.action_mailer.default_url_options = { :host => ENV["GMAIL_DOMAIN"] }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = false
