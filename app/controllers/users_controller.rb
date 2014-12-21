2279c4c996feedf2d08eb7ed3e1de3f01c63814b
Paul Darragh <pmd6624@rit.edu>
2014-12-21 16:21:28 -0500

    sort all users by first name

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -30,7 +30,7 @@ class UsersController < ApplicationController
   end
 
   def edit_all
-    @users = User.all.page(params[:page]).per_page(15)
+    @users = User.all.order(:first_name).page(params[:page]).per_page(15)
   end
 
   def update
