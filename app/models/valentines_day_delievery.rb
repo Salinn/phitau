869286c40414fac6ba8db33232d3939d092f989b
Paul Darragh <pmd6624@rit.edu>
2014-12-23 17:06:16 -0500

    cleaned up valentines day model a bit

--- app/models/valentines_day_delievery.rb
+++ app/models/valentines_day_delievery.rb
@@ -10,7 +10,7 @@ class ValentinesDayDelievery < ActiveRecord::Base
     (self.small_animial_quantity = 0) if small_animial_quantity.nil?
   end
 
-  PAYMENTMETHODS = ["Cash","PayPal"]
+  PAYMENTMETHODS = %w["Cash","PayPal"]
 
   validates :red_flower_quantity, :numericality => { :greater_than_or_equal_to => 0 }
   validates :white_flower_quantity, :numericality => { :greater_than_or_equal_to => 0 }
@@ -25,67 +25,67 @@ class ValentinesDayDelievery < ActiveRecord::Base
   validates :recievers_phone_number, presence: true, :length => {:minimum => 11}, :numericality => true
   validates :delivery_location, presence: true
 
-  validate :enough_red_flowers, :on => :create
-  validate :enough_white_flowers, :on => :create
-  validate :enough_pink_flowers, :on => :create
-  validate :enough_large_stuffed_animals, :on => :create
-  validate :enough_medium_stuffed_animals, :on => :create
-  validate :enough_small_stuffed_animals, :on => :create
+  validate :check_inventory, :on => [:create, :update]
 
-  def enough_red_flowers
+  def check_inventory
     inventory = ValentinesDayInventory.where(current_year: Time.now.year.to_s).first
+    enough_red_flowers inventory
+    enough_white_flowers inventory
+    enough_pink_flowers inventory
+
+    enough_large_stuffed_animals inventory
+    enough_medium_stuffed_animals inventory
+    enough_small_stuffed_animals inventory
+  end
+
+  def enough_red_flowers inventory
     if inventory.total_red_flowers-self.red_flower_quantity < 0
-      self.errors.add(:base, "There are not enough Red Flowers Currently")
+      self.errors.add(:base, 'There are not enough Red Flowers Currently')
       return false
     end
     inventory.total_red_flowers = inventory.total_red_flowers-self.red_flower_quantity
     inventory.save
     true
   end
-  def enough_white_flowers
-    inventory = ValentinesDayInventory.where(current_year: Time.now.year.to_s).first
+  def enough_white_flowers inventory
     if inventory.total_white_flowers-self.white_flower_quantity < 0
-      self.errors.add(:base, "There are not enough Pink Flowers Currently")
+      self.errors.add(:base, 'There are not enough Pink Flowers Currently')
       return false
     end
     inventory.total_white_flowers = inventory.total_white_flowers-self.white_flower_quantity
     inventory.save
     true
   end
-  def enough_pink_flowers
-    inventory = ValentinesDayInventory.where(current_year: Time.now.year.to_s).first
+  def enough_pink_flowers inventory
     if inventory.total_pink_flowers-self.pink_flower_quantity < 0
-      self.errors.add(:base, "There are not enough White Flowers Currently")
+      self.errors.add(:base, 'There are not enough White Flowers Currently')
       return false
     end
     inventory.total_pink_flowers = inventory.total_pink_flowers-self.pink_flower_quantity
     inventory.save
     true
   end
-  def enough_large_stuffed_animals
-    inventory = ValentinesDayInventory.where(current_year: Time.now.year.to_s).first
+  def enough_large_stuffed_animals inventory
     if inventory.total_large_stuffed_animials-self.large_stuffed_animial_quantity < 0
-      self.errors.add(:base, "There are not enough Large Stuffed Animals Currently")
+      self.errors.add(:base, 'There are not enough Large Stuffed Animals Currently')
       return false
     end
     inventory.total_large_stuffed_animials = inventory.total_large_stuffed_animials-self.large_stuffed_animial_quantity
     inventory.save
     true
   end
-  def enough_medium_stuffed_animals
-    inventory = ValentinesDayInventory.where(current_year: Time.now.year.to_s).first
+  def enough_medium_stuffed_animals inventory
     if inventory.total_medium_stuffed_animials-self.medium_stuffed_animial_quantity < 0
-      self.errors.add(:base, "There are not enough Medium Stuffed Animals Currently")
+      self.errors.add(:base, 'There are not enough Medium Stuffed Animals Currently')
       return false
     end
     inventory.total_medium_stuffed_animials = inventory.total_medium_stuffed_animials-self.medium_stuffed_animial_quantity
     inventory.save
     true
   end
-  def enough_small_stuffed_animals
-    inventory = ValentinesDayInventory.where(current_year: Time.now.year.to_s).first
+  def enough_small_stuffed_animals inventory
     if inventory.total_small_stuffed_animials-self.small_animial_quantity < 0
-      self.errors.add(:base, "There are not enough Small Stuffed Animals Currently")
+      self.errors.add(:base, 'There are not enough Small Stuffed Animals Currently')
       return false
     end
     inventory.total_small_stuffed_animials = inventory.total_small_stuffed_animials-self.small_animial_quantity
