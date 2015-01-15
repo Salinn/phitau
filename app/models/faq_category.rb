41ca0c1092affcfd748010264dc66ad8f67962d2
Tall Paul <pmd6624@rit.edu>
2015-01-15 11:09:47 -0500

    updates more validations

--- app/models/faq_category.rb
+++ app/models/faq_category.rb
@@ -1,3 +1,5 @@
 class FaqCategory < ActiveRecord::Base
   has_many :faq_questions
+
+  validates :title, length: {minimum: 4}
 end
--- app/models/faq_question.rb
+++ app/models/faq_question.rb
@@ -1,3 +1,7 @@
 class FaqQuestion < ActiveRecord::Base
   belongs_to :faq_categories
+
+  validates :title, length: {minimum: 4}
+  validates :content, length: {minimum: 10}
+  validates :faq_category_id, presence: true
 end
