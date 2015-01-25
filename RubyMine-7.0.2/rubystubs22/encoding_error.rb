7f0f3194036d38683ba406971fe8548b1499d1fb
salinn <pmd6624@rit.edu>
2015-01-24 21:35:59 -0500

    Deleted Rubymine files

--- RubyMine-7.0.2/rubystubs22/encoding_error.rb
+++
@@ -1,9 +0,0 @@
-=begin
- This is a machine generated stub using stdlib-doc for <b>class EncodingError</b>
- Sources used:  Ruby 2.2.0-preview2
- Created on 2014-12-04 11:56:59 +0300 by IntelliJ Ruby Stubs Generator.
-=end
-
-# EncodingError is the base class for encoding errors.
-class EncodingError < StandardError
-end
--- RubyMine-7.0.2/rubystubs22/sdbm_error.rb
+++
@@ -1,9 +0,0 @@
-=begin
- This is a machine generated stub using stdlib-doc for <b>class SDBMError</b>
- Sources used:  Ruby 2.2.0-preview2
- Created on 2014-12-04 11:56:59 +0300 by IntelliJ Ruby Stubs Generator.
-=end
-
-# Exception class used to return errors from the sdbm library.
-class SDBMError < StandardError
-end
