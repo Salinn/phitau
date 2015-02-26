0be6d4e8a74e955505fdddd81c83ace565df8a17
salinn <pmd6624@rit.edu>
2015-02-25 23:18:34 -0500

    updated security to community service

--- app/controllers/community_services_controller.rb
+++ app/controllers/community_services_controller.rb
@@ -1,5 +1,6 @@
 class CommunityServicesController < ApplicationController
   before_action :set_community_service, only: [:show, :edit, :update, :destroy]
+  load_and_authorize_resource
 
   respond_to :html
 
