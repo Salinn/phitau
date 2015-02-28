171b39346d6eb9fdba752acd0bad673b73eedf56
salinn <pmd6624@rit.edu>
2015-02-28 17:17:08 -0500

    fixed a few small issues with change over to positions

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -16,20 +16,21 @@ class UserMailer < ActionMailer::Base
   def new_user_alert_email(new_user)
     @new_user = new_user
 
-    user_id_to_mail_to = Chair.where(role: 'admin')
+    user_id_to_mail_to = Position.where(position_name: 'Web Master')
     if user_id_to_mail_to.nil?
-      user_id_to_mail_to = Eboard.where(role: 'president')
+      user_id_to_mail_to = Position.where(position_name: 'President')
     end
     @user = user_id_to_mail_to.first.user
     mail to: @user.email, subject: "New Person Signed Up On The Website #{new_user.first_name} #{new_user.last_name}"
   end
 
   def mailchimp_sign_up_user_email(user)
-    user_id_to_mail_to = Chair.where(role: 'alumni_relations')
-    if user_id_to_mail_to.nil?
-      user_id_to_mail_to = Eboard.where(role: 'president')
+    user_id_to_mail_to = Position.where(position_name: 'Alumni Relations')
+    position = Position.where(position_name: 'President')
+    if user_id_to_mail_to.nil? && position.user_id.blank?
+      user_id_to_mail_to = Position.where(position_name: 'President')
     else
-      user_id_to_mail_to = Chair.where(role: 'admin')
+      user_id_to_mail_to = Position.where(position_name: 'Web Master')
     end
     @user_to_contact = user_id_to_mail_to.first.user
     @user = user
