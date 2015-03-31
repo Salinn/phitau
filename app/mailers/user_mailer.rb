1a31a9695cde38770aa27a8ae29f614673899528
salinn <pmd6624@rit.edu>
2015-03-31 08:50:03 -0400

    working on improving emails

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -22,7 +22,10 @@ class UserMailer < ActionMailer::Base
     end
     @user = user_id_to_mail_to.user
     unless @user.nil?
-      mail to: @user.email, subject: "New Person Signed Up On The Website #{new_user.first_name} #{new_user.last_name}"
+      body_template = 'user_mailer/new_user_alert_email'
+      email_title = "New Person Signed Up On The Website #{new_user.first_name} #{new_user.last_name}"
+      to_address = @user.email
+      email_template(body_template, email_title, to_address)
     end
   end
 
@@ -40,9 +43,10 @@ class UserMailer < ActionMailer::Base
     mail to: @user.email, subject: "Hey #{user.first_name} Would You Like To Be On Our Mailing List?"
   end
 
-  def new_image_email user, image
+  def new_image_email(user, image)
     @user_who_submitted_the_image = user
     @image = image
+
 
     user_id_to_mail_to = Position.where(position_name: 'Web Master').first
     if user_id_to_mail_to.nil?
@@ -51,7 +55,28 @@ class UserMailer < ActionMailer::Base
     @user = user_id_to_mail_to.user
 
     unless @user.nil?
-      mail to: @user.email, subject: "New Image Submitted by #{@user_who_submitted_the_image.first_name} #{@user_who_submitted_the_image.last_name}"
+      body_template = 'user_mailer/new_user_alert_email'
+      email_title = "New Image Submitted by #{@user_who_submitted_the_image.first_name} #{@user_who_submitted_the_image.last_name}"
+      to_address = @user.email
+      email_template(body_template, email_title, to_address)
     end
   end
+
+  def email_template(body_template, email_title, to_address)
+    @body_template = body_template
+    subject = email_title
+
+    mail(to: to_address , subject: subject)
+  end
 end
+
+ActionMailer::Base.delivery_method = :smtp
+ActionMailer::Base.smtp_settings = {
+    address: "smtp.gmail.com",
+    port: 587,
+    domain: "localhost:3000",
+    authentication: "plain",
+    enable_starttls_auto: true,
+    user_name: ENV["GMAIL_USERNAME"],
+    password: ENV["GMAIL_PASSWORD"]
+}
