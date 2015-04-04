64363b50be9548803df1a69d840301221db8f636
salinn <pmd6624@rit.edu>
2015-04-04 15:57:49 -0400

    fixed issue with community service hours being nil

--- app/helpers/community_services_helper.rb
+++ app/helpers/community_services_helper.rb
@@ -2,6 +2,7 @@ module CommunityServicesHelper
   def total_hours(user)
     total = 0
     user.community_services.each do |cs|
+      next if cs.hours_volunteered.nil?
       total += cs.hours_volunteered
     end
     total
