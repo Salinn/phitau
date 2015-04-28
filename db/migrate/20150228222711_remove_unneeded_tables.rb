49dbda568427a74b1f6ba0fa2632257e0f191aab
Tall Paul <pmd6624@rit.edu>
2015-04-28 10:54:46 -0400

    fixed composites

--- db/migrate/20150228222711_remove_unneeded_tables.rb
+++ db/migrate/20150228222711_remove_unneeded_tables.rb
@@ -1,6 +1,6 @@
 class RemoveUnneededTables < ActiveRecord::Migration
   def change
-    drop_table :attendancespos
+    drop_table :attendances
     drop_table :meetings
     drop_table :user_positions
   end
