00ee6cacba2460e9ebf3c31e07f5714d5e52885d
salinn <pmd6624@rit.edu>
2015-08-05 22:05:28 -0400

    reformatted some seeds

--- db/seeds/image.rb
+++ db/seeds/image.rb
@@ -1,16 +1,94 @@
-Image.create!(id: 1, name: '', gallery_id: 1, post_id: 1, remote_picture_url: 'http://vignette3.wikia.nocookie.net/candh/images/7/70/Calvin%26HobbesDartWarGames.jpg/revision/latest?cb=20140223090538')
-Image.create!(id: 2, name: '', gallery_id: 1, post_id: 2, remote_picture_url: 'http://andculture.com/wp-content/uploads/2014/07/CalvinAndHobbes.jpg')
-Image.create!(id: 3, name: '', gallery_id: 1, post_id: 3, remote_picture_url: 'http://discover10.com/wp-content/uploads/2015/07/pictures-wallpapers.jpg')
-Image.create!(id: 4, name: '', gallery_id: 1, post_id: 4, remote_picture_url: 'http://blog.writeathome.com/wp-content/uploads/2011/09/calvin-and-hobbes.jpg')
-Image.create!(id: 5, name: '', gallery_id: 1, post_id: 5, remote_picture_url: 'http://mentalfloss.com/sites/default/files/calvin-hobbes-calvin-and-hobbes-23762778-1280-800_0.jpg')
-Image.create!(id: 6, name: '', gallery_id: 1, post_id: 6, remote_picture_url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRPJhXWN9GnuEHRzOSnKEi5wo26coCGXGwlKU28Hp0LkFo3iPml')
-Image.create!(id: 7, name: '', gallery_id: 1, post_id: 7, remote_picture_url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkOTogDPM5C6s2O5dsh-xkhydnFRFibssPBtnofkv8wSQ1U_3LNg')
-Image.create!(id: 8, name: '', gallery_id: 1, post_id: 8, remote_picture_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EpsV-NQnIX-XTQVtF3bCmokfjlvEWlWtT3dfDetV8dzOsCf2')
-Image.create!(id: 9, name: '', gallery_id: 1, post_id: nil, remote_picture_url: 'http://fc08.deviantart.net/fs71/i/2013/006/4/b/calvin_and_hobbes_graffiti_by_graffitiartist82-d5qmcp2.jpg')
-Image.create!(id: 10, name: '', gallery_id: 1, post_id: nil, remote_picture_url: 'http://images1.fanpop.com/images/photos/1300000/Calvin-and-Hobbes-calvin-and-hobbes-1395531-1024-768.jpg')
-Image.create!(id: 11, name: '', gallery_id: nil, post_id: nil, remote_picture_url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQxD5_X-5xs_P1njjnxjCk85J3a1pr8zaKi0-pgaokK3LjtuNI_')
-Image.create!(id: 12, name: '', gallery_id: nil, post_id: nil, remote_picture_url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS5_DW4WMwBwr9HwvHJLEUCmMR8TrD_P6mxhcS2SWlxsqO8bm-_')
-Image.create!(id: 13, name: '', gallery_id: nil, post_id: nil, remote_picture_url: 'http://images.fanpop.com/images/image_uploads/in-the-mud-calvin--26-hobbes-318677_1024_768.jpg')
-Image.create!(id: 14, name: '', gallery_id: nil, post_id: nil, remote_picture_url: 'http://static.comicvine.com/uploads/original/6/68762/1399647-calvin_and_hobbes_calvin_and_hobbes_1395529_1280_1024.jpg')
-Image.create!(id: 15, name: '', gallery_id: nil, post_id: nil, remote_picture_url: 'http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg')
-#Image.create!(id: 16, name: '', gallery_id: nil, post_id: nil, remote_picture_url: 'https://liberty21.org/wp-content/uploads/2015/03/Calvin-calvin-and-hobbes-30770695-200-200.jpg')
+images = [
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 1,
+        remote_picture_url: 'http://vignette3.wikia.nocookie.net/candh/images/7/70/Calvin%26HobbesDartWarGames.jpg/revision/latest?cb=20140223090538'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 2,
+        remote_picture_url: 'http://andculture.com/wp-content/uploads/2014/07/CalvinAndHobbes.jpg'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 3,
+        remote_picture_url: 'http://discover10.com/wp-content/uploads/2015/07/pictures-wallpapers.jpg'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 4,
+        remote_picture_url: 'http://blog.writeathome.com/wp-content/uploads/2011/09/calvin-and-hobbes.jpg'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 5,
+        remote_picture_url: 'http://mentalfloss.com/sites/default/files/calvin-hobbes-calvin-and-hobbes-23762778-1280-800_0.jpg'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 6,
+        remote_picture_url: 'https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRPJhXWN9GnuEHRzOSnKEi5wo26coCGXGwlKU28Hp0LkFo3iPml'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 7,
+        remote_picture_url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSkOTogDPM5C6s2O5dsh-xkhydnFRFibssPBtnofkv8wSQ1U_3LNg'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: 8,
+        remote_picture_url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT9EpsV-NQnIX-XTQVtF3bCmokfjlvEWlWtT3dfDetV8dzOsCf2'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: nil,
+        remote_picture_url: 'http://fc08.deviantart.net/fs71/i/2013/006/4/b/calvin_and_hobbes_graffiti_by_graffitiartist82-d5qmcp2.jpg'
+    },
+    {
+        name: '',
+        gallery_id: 1,
+        post_id: nil,
+        remote_picture_url: 'http://images1.fanpop.com/images/photos/1300000/Calvin-and-Hobbes-calvin-and-hobbes-1395531-1024-768.jpg'
+    },
+    {
+        name: '',
+        gallery_id: nil,
+        post_id: nil,
+        remote_picture_url: 'https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQxD5_X-5xs_P1njjnxjCk85J3a1pr8zaKi0-pgaokK3LjtuNI_'
+    },
+    {
+        name: '',
+        gallery_id: nil,
+        post_id: nil,
+        remote_picture_url: 'https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS5_DW4WMwBwr9HwvHJLEUCmMR8TrD_P6mxhcS2SWlxsqO8bm-_'
+    },
+    {
+        name: '',
+        gallery_id: nil,
+        post_id: nil,
+        remote_picture_url: 'http://images.fanpop.com/images/image_uploads/in-the-mud-calvin--26-hobbes-318677_1024_768.jpg'
+    },
+    {
+        name: '',
+        gallery_id: nil,
+        post_id: nil,
+        remote_picture_url: 'http://static.comicvine.com/uploads/original/6/68762/1399647-calvin_and_hobbes_calvin_and_hobbes_1395529_1280_1024.jpg'
+    },
+    {
+        name: '',
+        gallery_id: nil,
+        post_id: nil,
+        remote_picture_url: 'http://images1.fanpop.com/images/photos/1300000/Hobbes-calvin-and-hobbes-1395518-2000-1503.jpg'
+    }
+]
+
+Image.create!(images)
