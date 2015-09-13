214c10eb037cc08e6bb01283223e5b63f65d5780
salinn <pmd6624@rit.edu>
2015-09-13 15:25:42 -0400

    fixed rushing interviews page

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,8 @@
 class Event < ActiveRecord::Base
   belongs_to :user
 
-  validates :start_time, presence: true
-  validates :end_time, presence: true
+  # validates :start_time, presence: true
+  # validates :end_time, presence: true
   validates :event_date, presence: true
   validates :name, length: { minimum: 3 }
   validates :description, length: { minimum: 10 }
