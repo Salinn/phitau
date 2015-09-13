73655617a20f96abc97e9f82da2bb1afbc434641
salinn <pmd6624@rit.edu>
2015-09-13 19:42:41 -0400

    updated event seed

--- db/seeds/event.rb
+++ db/seeds/event.rb
@@ -12,19 +12,19 @@ events = [
     {
         name: 'Recruitment 2',
         description: 'Something that deals with recruitment',
-        start_time: (Time.now + 24.hours),
-        end_time: (Time.now + 26.hours),
+        start_time: (Time.now),
+        end_time: (Time.now + 2.hours),
         event_date: Date.today,
-        required_event: true,
-        private_event: false,
+        required_event: false,
+        private_event: true,
         user_id: 1
     },
     {
         name: 'Recruitment 3',
         description: 'Something that deals with recruitment',
-        start_time: (Time.now + 42.hours),
-        end_time: (Time.now + 44.hours),
-        event_date: Date.today,
+        start_time: Time.now,
+        end_time: (Time.now + 2.hours),
+        event_date: Date.today + 7.days,
         required_event: false,
         private_event: false,
         user_id: 1
@@ -32,19 +32,19 @@ events = [
     {
         name: 'Recruitment 4',
         description: 'Something that deals with recruitment',
-        start_time: (Time.now - 4.hours),
-        end_time: (Time.now - 2.hours),
-        event_date: Date.today,
+        start_time: Time.now,
+        end_time: (Time.now + 2.hours),
+        event_date: Date.today + 12.days,
         required_event: false,
-        private_event: true,
+        private_event: false,
         user_id: 1
     },
     {
         name: 'Recruitment 5',
         description: 'Something that deals with recruitment',
-        start_time: (Time.now - 72.hours),
-        end_time: (Time.now + 70.hours),
-        event_date: Date.today,
+        start_time: Time.now,
+        end_time: (Time.now + 2.hours),
+        event_date: Date.today + 17.days,
         required_event: false,
         private_event: false,
         user_id: 1
