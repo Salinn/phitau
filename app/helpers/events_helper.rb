828bc2375b6c9976f0c7e833a34bfd9a0692cdae
salinn <pmd6624@rit.edu>
2015-10-18 20:29:19 -0400

    updated event show and helper

--- app/helpers/events_helper.rb
+++ app/helpers/events_helper.rb
@@ -1,6 +1,14 @@
 module EventsHelper
-  def required?(event)
+  def event_required?(event)
     if event.required_event
+      'Required'
+    else
+      'Not Required'
+    end
+  end
+
+  def event_private(event)
+    if event.private_event
       'Required'
     else
       'Not Required'
