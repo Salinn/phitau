d306d125748463c2a5bef08123a6f586ea31d8e2
salinn <pmd6624@rit.edu>
2015-11-01 22:00:26 -0500

    galleries now have thumbnails

--- app/uploaders/image_uploader.rb
+++ app/uploaders/image_uploader.rb
@@ -36,6 +36,10 @@ class ImageUploader < CarrierWave::Uploader::Base
     process :resize_to_fit => [200, 200]
   end
 
+  version :profile_picture_thumb do
+    process :resize_to_fit => [250, 200]
+  end
+
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
   # def extension_white_list
